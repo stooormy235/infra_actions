0740d6c8efcf3077208e50badb35595357de1964
Хасан Циба <stooormy@yandex.ru>
2023-05-05 11:50:56 +0300

    kik

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -5,6 +5,7 @@ def index(request):
     return render(request, 'index.html',
                   context={'title': 'У меня получилось!'})
 
+
 def second_page(request):
     return render(request, 'second_page.html',
                   context={'title': 'А это вторая страница'})
