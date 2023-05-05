f318c229ffe4706c821b2c53cca02587bea4adf7
Хасан Циба <stooormy@yandex.ru>
2023-05-05 11:49:30 +0300

    rests

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'index.html', context={'title': 'У меня получилось!'})
+    return render(request, 'index.html',
+                  context={'title': 'У меня получилось!'})
 
 def second_page(request):
-    return render(request, 'second_page.html', context={'title': 'А это вторая страница'})
+    return render(request, 'second_page.html',
+                  context={'title': 'А это вторая страница'})
